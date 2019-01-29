339851b89d755febe5bc7b74f3fb7bcec53f0179
geoffroyvilain <vilaingeoffroy02@gmail.com>
2019-01-29 09:41:38 +0100

    exo fini

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+function makeBorder() {
+ document.getElementById("image1").style.borderWidth = "10px";
+ document.getElementById("image1").style.borderColor = "red";
+ document.getElementById("image1").style.border = "solid";
+
+
+}
+function unMakeBorder(){
+  document.getElementById("image1").style.borderWidth = "";
+  document.getElementById("image1").style.borderColor = "transparent";
+  document.getElementById("image1").style.border = "";
+}
