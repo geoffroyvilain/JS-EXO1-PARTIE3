1617167b9e2a8db98fed6e2f987748209c139e5c
geoffroyvilain <vilaingeoffroy02@gmail.com>
2019-01-31 09:15:37 +0100

    exo corrigé

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,8 @@
+//CETTE FONCTION PERMET D APPLIQUER UNE BORDURE SOLID ROUGE DE 3PX AU SURVOL DE L IMAGE
 function makeBorder() {
- document.getElementById("image1").style.borderWidth = "10px";
- document.getElementById("image1").style.borderColor = "red";
- document.getElementById("image1").style.border = "solid";
-
-
+ document.getElementById('image1').style.border = '3px solid red';
 }
+//CETTE FONCTION PERMET D ENLEVER LES BORDURES AU SURVOL DE L IMAGE
 function unMakeBorder(){
-  document.getElementById("image1").style.borderWidth = "";
-  document.getElementById("image1").style.borderColor = "transparent";
-  document.getElementById("image1").style.border = "";
+  document.getElementById('image1').style.border = '';
 }
